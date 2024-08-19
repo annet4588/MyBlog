3b59730fc4aef6e76ca4ebbbbd1f76174a2d7d85
annet4588 <annet4588@yahoo.co.uk>
2024-08-19 20:01:32 +0100

    Making Home Page, refactoring blades, setting home route

--- myBlog/resources/views/layouts/app.blade.php
+++ myBlog/resources/views/layouts/app.blade.php
@@ -16,11 +16,14 @@
         <!-- Styles -->
         @livewireStyles
     </head>
+    
     <body class="font-sans antialiased">
         <x-banner />
 
     <!-- Header -->
      @include('layouts.partials.header')
+
+     @yield('hero')
 
     <main class="container mx-auto px-5 flex flex-grow">
         {{$slot}}
